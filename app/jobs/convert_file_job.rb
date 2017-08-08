39f9a13bb1ed0b412dc3fcef6a47f517fe7550a6
solehudin <solehudin@alodokter.com>
2017-08-09 03:25:18 +0700

    fix update convert

--- app/jobs/convert_file_job.rb
+++ app/jobs/convert_file_job.rb
@@ -8,12 +8,12 @@ class ConvertFileJob < ActiveJob::Base
       text = File.read(upload_file.file_html.path)
       doc = Nokogiri::HTML.parse(text)
       doc.css('a').each do |link|
-        if link['href'].include?(params[:upload_file][:base_url])
-          base_url = link.attributes["href"].value.split('?partner=')[0]
-
-          if base_url[-1] != '/'
+        if link['href'].include?(params[:upload_file][:base_url]) || link['href'].include?(params[:upload_file][:base_url].chomp('/'))
+          if link.text != params[:upload_file][:base_url]
+            base_url = link.attributes["href"].value.split('&partner=')[0]
             new_url = (base_url.chomp('/') + '&partner=' + params[:upload_file][:partner])
           else
+            base_url = link.attributes["href"].value.split('?partner=')[0]
             new_url = (base_url.chomp('/') + '?partner=' + params[:upload_file][:partner])
           end
 
@@ -22,7 +22,7 @@ class ConvertFileJob < ActiveJob::Base
       end
 
       File.open(upload_file.file_html.path, "w") {|file| file.puts doc.to_s }
-      
+
       progress_bar.status = 'finish'
       progress_bar.save
     end
